7e496319b6030ce65b719e2e14b83939e936eca0
Wouterpiso <wpiso2000@gmail.com>
2022-06-21 22:40:57 +0200

    registering database 100% working

---
+++ Schoolvoetbal Win app/Schoolvoetbal Win app/PasswordHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ultimehoofdpijn_2_electric_boogaloo_FEAT_melancholie
+{
+    public class PasswordHelper
+    {
+        public static string Salt { get; set; } = "yablablo";
+
+        public static bool CheckHash(string input, string expected, string salt = "")
+        {
+            var hashedInput = GetSha256Hash(input, salt);
+
+            return hashedInput == expected;
+        }
+
+        public static string GetSha256Hash(string input, string salt = "")
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(input + salt));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+
+            return hash.ToString();
+        }
+
+        public static void Example()
+        {
+            string password = "een wachtwoord";
+            string salt = "yablablo";
+            string passwordHashWithoutSalt = GetSha256Hash(password);
+
+            bool passwordCorrect = CheckHash(password, passwordHashWithoutSalt);
+
+            string passwordHashWithSalt = GetSha256Hash(password, salt);
+
+            passwordCorrect = CheckHash(password, passwordHashWithSalt, salt);
+        }
+    }
+}
--- Schoolvoetbal Win app/Schoolvoetbal Win app/Registerform.cs
+++ Schoolvoetbal Win app/Schoolvoetbal Win app/Registerform.cs
@@ -20,7 +20,7 @@ namespace ultimehoofdpijn_2_electric_boogaloo_FEAT_melancholie
         {
             InitializeComponent();
             connection = new MySqlConnection(connStr);
-            //connection.Open();
+            connection.Open();
         }
 
         private void Btnregister_Click(object sender, EventArgs e)
@@ -35,37 +35,76 @@ namespace ultimehoofdpijn_2_electric_boogaloo_FEAT_melancholie
 
             try
             {
-                string qry = $"INSERT INTO users(NAME,email,PASSWORD) VALUES('{accountName}','{accountemail}', '{accountPassword}');";
-                MySqlCommand command = new MySqlCommand(qry, connection);
+                bool useHashing = true;
+
+                MySqlParameter paramName;
+                MySqlParameter paramEmail;
+                MySqlParameter paramPassword;
+
+                paramName = new MySqlParameter("user", accountName);
+                paramEmail = new MySqlParameter("email", accountemail);
+
+                if (useHashing)
+                {
+                    string passwordHash = PasswordHelper.GetSha256Hash(accountPassword, PasswordHelper.Salt);
+                    paramPassword = new MySqlParameter("password", passwordHash);
+                    
+                    string getPasswordQury = "SELECT password from users where name = @user";
+                    MySqlCommand getPasswordCommand = new MySqlCommand(getPasswordQury, connection);
+                    getPasswordCommand.Parameters.Add(paramName);
+                    
+                    string opgehaaldWachtwoord = getPasswordCommand.ExecuteScalar() as string;
+                    bool passwordCorrect = PasswordHelper.CheckHash(accountPassword, opgehaaldWachtwoord, PasswordHelper.Salt);
+                }
+                else
+                {
+                    paramPassword = new MySqlParameter("password", accountPassword);
+                }
+
+                string insertUserQry = $"INSERT INTO users(NAME,email,PASSWORD) VALUES(@user, @email, @password);";
+                string updateUserQry = $"UPDATE users set NAME = @user, email = @email, password = @password where name = @name";
+
+
+                MySqlCommand command = new MySqlCommand(insertUserQry, connection);
+                command.Parameters.Add(paramName);
+                command.Parameters.Add(paramEmail);
+                command.Parameters.Add(paramPassword);
+
                 if (connection.State != ConnectionState.Open)
                 {
                     connection.Open();
 
                 }
+
                 int rowsAdded = command.ExecuteNonQuery();
-                connection.Close();
 
                 if (rowsAdded <= 0)
                 {
                     throw new Exception("Geen rij toegevoegd");
                 }
+
                 string accountadmin = "0";
                 string accountMoney = "50";
                 AccountsList.Add(new Accounts(accountName, accountPassword, accountadmin, accountMoney, accountemail));
-                MessageBox.Show("Account voor" + accountName + " gemaakt");
+                MessageBox.Show("Account voor " + accountName + " gemaakt");
                 MainForm frmMain = new MainForm(accountName, accountadmin, accountMoney);
                 frmMain.Show();
                 this.Close();
+
             }
             catch(Exception ex)
             {
                 MessageBox.Show("iets is er heel fout gegaan: " + ex.Message);
             }
+            finally
+            {
+                connection?.Close();
+            }
         }
 
         private void Back_Click(object sender, EventArgs e)
         {
-           Form1 form = new Form1();
+            Form1 form = new Form1();
             form.Show();
             this.Close();
         }
