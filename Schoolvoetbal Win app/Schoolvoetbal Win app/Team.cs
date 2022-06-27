c3827530d1189cdc450b88802bd6f204dd16eb47
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2022-06-27 09:51:01 +0200

    Begin form

---
+++ Schoolvoetbal Win app/Schoolvoetbal Win app/Team.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ultimehoofdpijn_2_electric_boogaloo_FEAT_melancholie
+{
+    public partial class Team : Form
+    {
+        public Team()
+        {
+            InitializeComponent();
+        }
+    }
+}
