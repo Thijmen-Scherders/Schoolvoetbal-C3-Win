10c5868f18ecad8b6387a85087f0e2ae1bf78c3c
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2022-06-27 12:17:13 +0200

    push

--- Schoolvoetbal Win app/Schoolvoetbal Win app/Team.cs
+++ Schoolvoetbal Win app/Schoolvoetbal Win app/Team.cs
@@ -16,5 +16,10 @@ namespace ultimehoofdpijn_2_electric_boogaloo_FEAT_melancholie
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
