2c8fb523516a185145671caeaac34a618479d38c
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2022-06-29 12:11:24 +0200

    Stomme push

--- Schoolvoetbal Win app/Schoolvoetbal Win app/Team.Designer.cs
+++ Schoolvoetbal Win app/Schoolvoetbal Win app/Team.Designer.cs
@@ -51,7 +51,7 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(347, 276);
             this.listBox1.TabIndex = 1;
-           
+            
             // 
             // label1
             // 
